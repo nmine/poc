3742c1919bab64d9bd7b7c5eb0ac8aa4f230892d
Nicolas Mine <nicolas.mine11@gmail.com>
2014-02-11 00:27:06 +0100

    rm duplicate project

--- pocgwt263/src/main/java/be/nmine/client/views/event/HasChangeColorEventHandlers.java
+++
@@ -1,9 +0,0 @@
-package be.nmine.client.views.event;
-
-import com.google.gwt.event.shared.HandlerRegistration;
-import com.google.gwt.event.shared.HasHandlers;
-
-public interface HasChangeColorEventHandlers extends HasHandlers {
-	public HandlerRegistration addHasChangeColorEventHandler(
-			ChangeColorEventHandler handler);
-}
